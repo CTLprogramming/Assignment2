84030c5ce81c04605de799c5b928191507537fd6
Ceallach Levins <30018942@live.nwrc.ac.uk>
2023-04-30 21:46:56 +0100

    Resources embedded

--- BookShop2/Properties/Resources.Designer.cs
+++ BookShop2/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace BookShop2.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace BookShop2.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BookShop2.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace BookShop2.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace BookShop2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ACB_logo2 {
+        public static System.Drawing.Bitmap ACB_logo2 {
             get {
                 object obj = ResourceManager.GetObject("ACB_logo2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -73,7 +73,7 @@ namespace BookShop2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ACB_logo3 {
+        public static System.Drawing.Bitmap ACB_logo3 {
             get {
                 object obj = ResourceManager.GetObject("ACB_logo3", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
@@ -83,7 +83,7 @@ namespace BookShop2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap btnLogo {
+        public static System.Drawing.Bitmap btnLogo {
             get {
                 object obj = ResourceManager.GetObject("btnLogo", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
